75cfe595fcf9f133cef39b79656d4baded69311f
Lucas R. de Souza <LucasRSouza3500@gmail.com>
2024-12-13 17:12:37 -0300

    Add files via upload

--- server.js
+++ server.js
@@ -19,9 +19,11 @@ app.use(cors());
 
 // DATABASE
 export const pool = mysql.createPool({
+    host: process.env.DB_HOST,
     user: process.env.DB_USER,
     password: process.env.DB_PASSWORD,
     database: process.env.DB,
+    port: process.env.DB_PORT,
     multipleStatements: true
 }).promise();
 
